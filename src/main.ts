c2b88625b81d5a4baced3fd0136ce0e2d61103e1
Bruno Santana <brunoosouza15@gmail.com>
2024-06-22 15:57:11 -0300

    feature: implement scalar api reference (#28)

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,15 @@
-import 'module-alias/register';
-import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import {
-  SwaggerModule,
-  DocumentBuilder,
-  SwaggerCustomOptions,
-} from '@nestjs/swagger';
-import { AppModule } from '@/app.module';
+import "module-alias/register";
+import { AppModule } from "@/app.module";
 import {
   HttpExceptionFilter,
   PrismaClientExceptionFilter,
-} from '@/infra/http/nest/@config/filter-exceptions';
-import { ServerVariableObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
-import { Env } from './env';
-
-const nodeEnvServerVariable: ServerVariableObject = {
-  default: 'dev',
-  description: 'Node env variable',
-  enum: ['test', 'dev', 'prod'],
-};
-
-const serversVariable = { NODE_ENV: nodeEnvServerVariable };
+} from "@/infra/http/nest/@config/filter-exceptions";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
+import { Env } from "./env";
 
 async function bootstrap() {
   /* Set config initial */
@@ -29,34 +17,47 @@ async function bootstrap() {
   const configService: ConfigService<Env, true> = app.get(ConfigService);
 
   /* Add consts */
-  const GLOBAL_PREFIX = configService.get('GLOBAL_PREFIX');
-  const SWAGGER_PREFIX = configService.get('SWAGGER_PREFIX');
-  const API_PORT = configService.get('API_PORT');
+  const GLOBAL_PREFIX = configService.get("GLOBAL_PREFIX");
+  const SWAGGER_PREFIX = configService.get("SWAGGER_PREFIX");
+  const API_PORT = configService.get("API_PORT");
 
   /* Set endpoint */
   app.setGlobalPrefix(GLOBAL_PREFIX);
 
   /* Swagger Setup */
   const config = new DocumentBuilder()
-    .setTitle('ACOMASQ API')
+    .setTitle("ACOMASQ API")
     .setDescription(
-      'API criada para cadastro e gerenciamento de usuários e pagamentos de água na ACOMASQ',
+      "API criada para cadastro e gerenciamento de usuários e pagamentos de água na ACOMASQ",
     )
-    .setVersion('0.0.1')
-    .addServer(`http://localhost:3003`, 'DEV', serversVariable)
-    .addServer('https://acomasq-api.com', 'STAGE', serversVariable)
+    .setVersion("0.0.1")
+    .addServer(`http://localhost:3003`, "DEV", {
+      DEV: {
+        description: "dev environment to use on development",
+        default: false,
+      },
+    })
+    .addServer("https://acomasq-api.com", "STAGE", {
+      STAGE: { description: "stage environment", default: true },
+    })
     .addBearerAuth()
     .build();
 
-  const customOptions: SwaggerCustomOptions = {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  };
-
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup(SWAGGER_PREFIX, app, document, customOptions);
+  app.use(
+    "/docs",
+    apiReference({
+      theme: "bluePlanet",
+      darkMode: true,
+      layout: "classic",
+      hideModels: false,
+      showSidebar: true,
+      spec: {
+        content: document,
+      },
+    }),
+  );
 
   /* Set validation config */
   app.useGlobalFilters(
